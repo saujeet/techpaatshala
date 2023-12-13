0c647f5ed21629c251f69454e54e1f69c7779e15
Saujeet Sarkar <saujeetsarkar@gmail.com>
2023-12-13 13:24:42 +0530

    updated js concepts

--- sessions/js_concepts.js
+++ sessions/js_concepts.js
@@ -4,7 +4,7 @@ let hours = 11;
 let day = "Wednesday";
 let month = "December";
 
-let today = `Deadline is at ${hours} on ${day}, ${month}`;
+let today = `Deadline is at ${hours} AM on ${day}, ${month}`;
 console.log(today);
 
 // Spread
@@ -13,3 +13,21 @@ let even = [2, 4, 6, 8, 10];
 let odd = [1, 3, 5, 7, 9];
 let evenodd = [...odd, ...even];
 console.log(evenodd.sort(function (a, b) { return a - b; }));
+
+// arrow functions
+
+let num = 5;
+let square = num => num * num;
+console.log(square);
+
+// destructor
+
+let person = {
+    name: 'John',
+    age: 34,
+}
+
+const { name, age } = person;
+
+// array methods
+
